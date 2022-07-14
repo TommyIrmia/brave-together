4d6adcbde2b500d9a27b591dce1c90f6ba6b5fa3
Tommy <tommyirmia@gmail.com>
2022-07-14 13:47:31 +0300

    added data format

--- client/src/storyUpload/StoryUpload.js
+++ client/src/storyUpload/StoryUpload.js
@@ -18,28 +18,28 @@ class StoryUpload extends React.Component {
     };
   }
 
-  getCurrDate(){
-   
+  getCurrDate() {
+
 
 
 
   }
 
   componentDidMount() {
-    
-    
+
+
     var today = new Date();
 
     let year = today.getFullYear();
     let month = (today.getMonth() + 1).toString().padStart(2, "0");
     let day = today.getDate();
 
-    this.setState({date: year + "-" + month +  "-" + day}, () => {
+    this.setState({ date: year + "-" + month + "-" + day }, () => {
       console.log("date is " + this.state.date)
     })
-    
+
     // let token = localStorage.getItem('token');
-    
+
     // console.log("in mount")   
     // if (!token) {
     //     this.props.history.push('/')
@@ -57,17 +57,17 @@ class StoryUpload extends React.Component {
     //             this.props.history.push('/login');
     //         }
     //     });
-    }
-    
+  }
 
-    // this.setState({stories: mock});
-// };
 
-// async handleSubmit(event) {
+  // this.setState({stories: mock});
+  // };
 
-  handleSubmit = async(event) => {
+  // async handleSubmit(event) {
+
+  handleSubmit = async (event) => {
     event.preventDefault();
-    
+
     console.log("button clicked");
 
     const storyDocument = {
@@ -77,24 +77,26 @@ class StoryUpload extends React.Component {
       "date": this.state.date,
       "tags": this.state.tags,
       "country": this.state.country
-  };
+    };
 
     let axiosConfig = {
-      auth: {username: 
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwiZXhwIjoxNjMyNjg3Nzk5Ljk5NTIzM30.jFeXH4uyqJXXvYbcng8CfxYsl2pG6CMtYm8imao-X4Y"}     
-      };
+      auth: {
+        username:
+          "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwiZXhwIjoxNjMyNjg3Nzk5Ljk5NTIzM30.jFeXH4uyqJXXvYbcng8CfxYsl2pG6CMtYm8imao-X4Y"
+      }
+    };
 
     console.log(storyDocument);
     axios.post("http://localhost:5000/stories", storyDocument, axiosConfig) // calling api request to create a new story
-    .then(response =>{
-         console.log(response);
-         alert("!הסיפור הועלה בהצלחה");
-         this.props.history.push("/");
-    })   
-    .catch ( err =>{
-      console.log(err)
-      this.props.history.push("/storyUpload");
-    })
+      .then(response => {
+        console.log(response);
+        alert("!הסיפור הועלה בהצלחה");
+        this.props.history.push("/");
+      })
+      .catch(err => {
+        console.log(err)
+        this.props.history.push("/storyUpload");
+      })
   };
 
 
@@ -115,25 +117,25 @@ class StoryUpload extends React.Component {
         <form id="form">
           <label for="title">כותרת</label>
           <br></br>
-          <input type="text" id="title" name="title" onChange={this.handleInputChanges} required/>
+          <input type="text" id="title" name="title" onChange={this.handleInputChanges} required />
           <br />
           <br />
           <label for="story" className="content">תוכן</label>
           <br></br>
           <textarea id="story" name="text" rows="6" cols="23" onChange={this.handleInputChanges} required="required"
-           style={{ resize: "none" }}  />
+            style={{ resize: "none" }} />
           <br />
           <label for="title">תגיות</label>
           <br></br>
-          <input type="text" id="tags"name="tags" onChange={this.handleTagsChange}/>
+          <input type="text" id="tags" name="tags" onChange={this.handleTagsChange} />
           <p>רשמו מספר מילים מרכזיות מהסיפור על מנת לצבור חשיפה גבוהה יותר</p>
           <br></br>
           <div id="button">
-          <button type="submit" onClick={this.handleSubmit}
-            style={{
-              borderRadius: "8px"
-            }}
-          >העלאת הסיפור</button>
+            <button type="submit" onClick={this.handleSubmit}
+              style={{
+                borderRadius: "8px"
+              }}
+            >העלאת הסיפור</button>
           </div>
         </form>
       </div>
