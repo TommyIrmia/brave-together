689115c763d1c26c50a6e23b9ad6d7c178ec7fba
Avi Iskaov <avitech1242@gmail.com>
2022-07-25 23:48:32 +0300

    designing basic quote structure. adding quote-btns-container cmps

---
+++ client/src/cmps/quote/quote-btns.jsx
@@ -0,0 +1,8 @@
+export const QuoteBtns = () => {
+  return (
+    <div className="quote-btns-container">
+      <button>תיעוד גיבור/ה</button>
+      <button>יהודי מציל יהודי</button>
+    </div>
+  )
+}
--- client/src/cmps/quote/quote-preview.jsx
+++ client/src/cmps/quote/quote-preview.jsx
@@ -1,14 +1,25 @@
+import { QuoteBtns } from './quote-btns'
+
 export const QuotePreview = ({ quote }) => {
   return (
-    <div className="quote-preview">
-      <p className="quote-content" style={getQuoteStyle(quote.txt)}>
-        {quote.txt.content}
-      </p>
+    <div className="quote-preview" style={getQuoteStyle(quote.background)}>
+      <div className="quote-content" style={getQuoteContentStyle(quote.txt)}>
+        <p className="quote-text">{quote.txt.content}</p>
+        <p className="hero-name">-{quote.story.heroName}-</p>
+      </div>
+      <QuoteBtns />
     </div>
   )
 }
 
-const getQuoteStyle = ({ fontColor, fontFamily, fontSize }) => {
+const getQuoteStyle = ({ attr }) => {
+  console.log(attr)
+  return {
+    backgroundColor: attr || '#E05555',
+  }
+}
+
+const getQuoteContentStyle = ({ fontColor, fontFamily, fontSize }) => {
   return {
     color: fontColor || '#000',
     fontFamily: fontFamily || 'Rubik',
