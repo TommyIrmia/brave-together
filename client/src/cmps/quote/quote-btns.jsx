9f9e24f18a56bb8f6b4195dd35785d7a6d143c89
Avi Iskaov <avitech1242@gmail.com>
2022-07-30 22:58:21 +0300

    connecting quote-tags to backend

--- client/src/cmps/quote/quote-btns.jsx
+++
@@ -1,8 +0,0 @@
-export const QuoteBtns = () => {
-  return (
-    <div className="quote-btns-container">
-      <button>תיעוד גיבור/ה</button>
-      <button>יהודי מציל יהודי</button>
-    </div>
-  )
-}
--- client/src/cmps/quote/quote-preview.jsx
+++ client/src/cmps/quote/quote-preview.jsx
@@ -1,7 +1,20 @@
-import { QuoteBtns } from './quote-btns'
+import { QuoteTags } from './quote-tags'
 import { QuoteToolBar } from './quote-toolbar'
+import { storyService } from '../../services/api/story.service'
+import { useEffect, useState } from 'react'
 
 export const QuotePreview = ({ quote }) => {
+  const [tags, setTags] = useState([])
+
+  useEffect(() => {
+    getStoryTags(quote.story._id)
+  }, [])
+
+  const getStoryTags = async (storyId) => {
+    const story = await storyService.getById(storyId)
+    return setTags(story.tags)
+  }
+
   return (
     <div className="quote-preview" style={getQuoteStyle(quote.background)}>
       <div className="quote-content" style={getQuoteContentStyle(quote.txt)}>
@@ -9,7 +22,7 @@ export const QuotePreview = ({ quote }) => {
         <p className="hero-name">-{quote.story.heroName}-</p>
       </div>
       <section className="quote-footer">
-        <QuoteBtns />
+        <QuoteTags tags={tags} />
         <QuoteToolBar />
       </section>
     </div>
