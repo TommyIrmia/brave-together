92ea102065818f72898d971c957e37947e397905
Tommy <tommyirmia@gmail.com>
2022-08-18 13:28:54 +0300

    fixed

--- client/src/cmps/quote/quote-edit/canvas-container.jsx
+++ client/src/cmps/quote/quote-edit/canvas-container.jsx
@@ -126,21 +126,13 @@ export const CanvasContainer = ({ canvasRef, ctxRef, onUpdateQuote, onToggleShar
                     </div>
                     שמור
                 </button>
-
-
-
-
-
-
-
-
             </a >
         </div >
+
         <span>
             Everything here is your local code!
             The code I worked on and commited the changes to git
         </span>
-
 
 
 
